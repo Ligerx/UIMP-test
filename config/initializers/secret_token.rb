60296ccd5b2faa22dcc1e0503230a4c2f15b86ae
awang1 <awang1@andrew.cmu.edu>
2015-02-14 17:52:42 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UIMPTest::Application.config.secret_key_base = '1f8ca6e5b596b6b56822168b418abb3400a9b61805a7f28b567b6052251fe128660e91549939517fe049684c844ab16192d2c4ef759945d0cf3f82d15a3a1e38'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UIMPTest::Application.config.session_store :cookie_store, key: '_UIMP_test_session'
