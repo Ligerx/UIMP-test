92eee4692bccb5614cfbd358fcd13cbae79bf7c8
awang1 <awang1@andrew.cmu.edu>
2015-03-15 22:30:08 -0400

    Create token model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150216053534) do
+ActiveRecord::Schema.define(version: 20150316022010) do
+
+  create_table "tokens", force: true do |t|
+    t.string   "access_token"
+    t.string   "user_id"
+    t.datetime "expiration_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
