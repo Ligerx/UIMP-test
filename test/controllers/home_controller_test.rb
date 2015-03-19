1718e74e557b904f51c1b0ab6896b69763eda30b
awang1 <awang1@andrew.cmu.edu>
2015-03-19 02:07:12 -0400

    test home controller

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,18 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
   test "should get index" do
     get :index
     assert_response :success
   end
 
+  test "should contain login and sign up button" do
+    get :index
+    assert_response :success
+
+    assert_select "a", href: "/user/sign_up", text: "Sign up"
+    assert_select "a", href: "/user/sign_in", text: "Login"
+  end
 end
