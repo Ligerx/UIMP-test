8b1e734b24875d8ae8e8dee253fa7ca30b68872e
awang1 <awang1@andrew.cmu.edu>
2015-04-02 02:15:39 -0400

    Fix bug in account controller change password and test it more

--- app/controllers/uimp/account_controller.rb
+++ app/controllers/uimp/account_controller.rb
@@ -22,7 +22,7 @@ Rails::logger.debug "USER: #{user}"
       render json: {error_code: 2, error_description: "Invalid credentials"} and return
     end
 
-    if user.update_attributes(change_password_params)
+    if user.update(password: @new_password)
       render json: {} and return
     else
       render json: {error_code: 3, error_description: "Error updating password"} and return
@@ -34,44 +34,50 @@ Rails::logger.debug "USER: #{user}"
 
   private
   def change_password_params
-    # Devise requires these particular key names.
-    # Doing it this way is simpler than renaming through strong parameters.
-    # allowed_values = {email: params[:user_id], current_password: params[:old_password], password: params[:new_password]}
-
-    # params.permit(:email, :current_password, :password)
-    # params.permit(:user_id, :old_password, :new_password)
-    params.permit(:email, :current_password, :password, :user_id, :old_password, :new_password)
-
-    reformat_params
-
-    params
-  end
-
-  def reformat_params
-    params[:email] = params.delete(:user_id) if params[:user_id]
-    params[:current_password] = params.delete(:old_password) if params[:old_password]
-    params[:password] = params.delete(:new_password) if params[:new_password]
+    params.permit(:access_token, :password, :user_id, :old_password, :new_password)
   end
 
   def find_user
     token_param = change_password_params[:access_token]
-    email_param = change_password_params[:email]
-    password_param = change_password_params[:current_password]
+    email_param = change_password_params[:user_id]
+    email_old_password = change_password_params[:old_password]
+
+Rails::logger.debug "INSIDE FIND_USER"
+Rails::logger.debug "token #{token_param},    email #{email_param},     old password #{email_old_password}"
+
 
     if token_param
+Rails::logger.debug "INSIDE FIND_USER TOKEN"
+
       # Check for a non expired token, and then find its user
       token = Token.find_by_access_token(token_param)
-      return nil if token.expired?
+      @new_password = change_password_params[:password]
 
-      User.find_by_email(token.user_id)
-    elsif email_param && password_param
+      return nil if token.nil? || token.expired?
+
+      return User.find_by_email(token.user_id)
+
+    elsif email_param && email_old_password
+Rails::logger.debug "INSIDE FIND_USER email+password"
+
       # Find a user and check that user exists and has the right password
       user = User.find_by_email(email_param)
-      return nil unless user && user.valid_password?(password_param)
+      @new_password = change_password_params[:new_password]
 
-      user
+Rails::logger.debug "user: #{user}"
+Rails::logger.debug "@new_password: #{@new_password}"
+Rails::logger.debug "user.nil? #{user.nil?}"
+Rails::logger.debug "valid_password? #{user.valid_password?(email_old_password)}"
+
+
+
+      return nil if user.nil? || !user.valid_password?(email_old_password)
+
+      return user
+
     end
 
+Rails::logger.debug "wtf return nil at end of method"
     return nil
   end
 
--- test/controllers/uimp/account_controller_test.rb
+++ test/controllers/uimp/account_controller_test.rb
@@ -3,27 +3,45 @@ require 'test_helper'
 class Uimp::AccountControllerTest < ActionController::TestCase
   include Devise::TestHelpers
 
-  test "should change password" do
-    # put :change_password, {'user_id' => 'Alex@test.com', 'old_password' => 'password', 'new_password' => 'new_password'}
+  test "should change password with user_id and old password" do
     put :change_password, {user_id: 'Alex@test.com', old_password:'password', new_password:'new_password'}
     assert_response :success
 
     user = users(:Alex)
-    # sign_in user
 
     assert user.valid_password?('new_password'), "#{get_json_from @response.body}"
     assert_not user.valid_password? 'password'
   end
 
-  test "should NOT change password" do
+  test "should change password with token" do
+    put :change_password, {access_token: tokens(:one).access_token, password: 'new_password'}
+    assert_response :success
+
+    user = users(:Alex)
+
+    assert user.valid_password?('new_password'), "#{get_json_from @response.body}"
+    assert_not user.valid_password? 'password'
+  end
+
+  test "should not change password given the wrong password" do
     put :change_password, {'user_id' => 'Bob@test.com', 'old_password' => 'wrong_password', 'new_password' => 'new_password'}
     assert_response :success
 
     user = users(:Bob)
-    sign_in user
 
     assert user.valid_password? 'password2'
     assert_not user.valid_password? 'wrong_password'
     assert_not user.valid_password? 'new_password'
   end
+
+  test "should not change password given expired token" do
+    put :change_password, {access_token: tokens(:three).access_token, password: 'new_password'}
+    assert_response :success
+
+    user = users(:Bob)
+
+    assert user.valid_password? 'password2'
+    assert_not user.valid_password? 'new_password'
+  end
+
 end
