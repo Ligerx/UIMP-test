a7f040bd4ccc86f38084bafabbe402e74acdb98a
awang1 <awang1@andrew.cmu.edu>
2015-04-20 00:30:22 -0400

    Added basic validations and relation to notification

--- app/models/uimp/notification.rb
+++ app/models/uimp/notification.rb
@@ -1,2 +1,7 @@
 class Uimp::Notification < ActiveRecord::Base
+  belongs_to :user
+
+  # does not include the <UIMP API name> events right now
+  validates_inclusion_of :event, in: %w[always invalid_client_token login_success login_success_without_client_id login_failure]
+  validates_inclusion_of :medium_type, in: %w[email sms]
 end
